86084b894313b192326ed06e64e8f33453b8ac7d
Viggeeeh <viggo.ofors@elev.ga.ntig.se>
2023-11-14 10:17:52 +0100

    yess

--- Portfolio/app.js
+++ Portfolio/app.js
@@ -1,4 +1,4 @@
-const createCard = (headerText, content) => {
+const createCard = (headerText, content, link) => {
     // Create tags for html
     const projects = document.querySelector(".projects-container")
     const div = document.createElement("div")
@@ -17,11 +17,11 @@ const createCard = (headerText, content) => {
 }
 
 // create cards
-createCard("Header here!", "Hellooooo")
-createCard("Hello!", "Hellooooo")
-createCard("Hello!", "Hellooooo")
-createCard("Hello!", "Hellooooo")
-createCard("Hello!", "Hellooooo")
-createCard("Hello!", "Hellooooo")
+createCard("Uppgift 1", "Hellooooo", "../Uppgift")
+createCard("Uppgift 2", "Hellooooo")
+createCard("Uppgift 3", "Hellooooo")
+createCard("Uppgift 4", "Hellooooo")
+createCard("Uppgift 5", "Hellooooo")
+createCard("Uppgift 6", "Hellooooo")
 
 
