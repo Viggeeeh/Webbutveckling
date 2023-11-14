344f8a923dbdd9668826a4134b7d4b4dc39f540d
Viggeeeh <viggo.ofors@elev.ga.ntig.se>
2023-11-14 10:06:55 +0100

    yes

--- Portfolio/app.js
+++ Portfolio/app.js
@@ -1,6 +1,27 @@
-const CTA = document.getElementById("contact")
+const createCard = (headerText, content) => {
+    // Create tags for html
+    const projects = document.querySelector(".projects-container")
+    const div = document.createElement("div")
+    const header = document.createElement("h1")
+    const paragraph = document.createElement("p")
 
-CTA.addEventListener('click', () => {
-    console.log("Jellow!")
-}) 
-    
+    // Creates the card
+    div.classList.add("project-card")
+    header.innerHTML = headerText
+    div.appendChild(header)
+    projects.appendChild(div)
+
+    paragraph.classList.add("information")
+    paragraph.innerHTML = content  
+    div.appendChild(paragraph)
+}
+
+// create cards
+createCard("Header here!", "Hellooooo")
+createCard("Hello!", "Hellooooo")
+createCard("Hello!", "Hellooooo")
+createCard("Hello!", "Hellooooo")
+createCard("Hello!", "Hellooooo")
+createCard("Hello!", "Hellooooo")
+
+
