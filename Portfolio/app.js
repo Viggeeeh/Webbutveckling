6241a2d8f136580fb0d7eee56036fcdf51da7eda
Viggeeeh <viggo.ofors@elev.ga.ntig.se>
2023-11-29 10:08:55 +0100

    portfolio klar

--- Portfolio/app.js
+++ Portfolio/app.js
@@ -1,8 +1,9 @@
-const createCard = (headerText, content, idName) => {
+const createCard = (headerText, content, idName, imgLink) => {
     // Create tags for html
     const projects = document.querySelector(".projects-container")
     const div = document.createElement("div")
     const header = document.createElement("h1")
+    const img = document.createElement("img")
     const paragraph = document.createElement("p")
 
     // Creates the card
@@ -11,6 +12,10 @@ const createCard = (headerText, content, idName) => {
     header.innerHTML = headerText
     div.appendChild(header)
     projects.appendChild(div)
+
+    div.appendChild(img)
+    img.classList.add("projects-image")
+    img.src = imgLink
 
     paragraph.classList.add("information")
     paragraph.innerHTML = content  
@@ -25,12 +30,12 @@ const isClicked = (id, link) => {
 }
 
 // create cards
-createCard("Uppgift 1", "Hellooooo", "uppgift-1")
-createCard("Uppgift 2", "Hellooooo", "uppgift-2")
-createCard("Uppgift 3", "Hellooooo", "uppgift-3")
-createCard("Uppgift 4", "Hellooooo", "uppgift-4")
-createCard("Uppgift 5", "Hellooooo", "uppgift-5")
-createCard("Uppgift 6", "Hellooooo", "uppgift-6")
+createCard("Uppgift 1", "Introduktion till HTML och CSS", "uppgift-1", "./images/uppgift-1.png")
+createCard("Uppgift 2", "Vi gick igenom Div och IMG", "uppgift-2", "./images/uppgift-2.png")
+createCard("Uppgift 3", "Användning utav Flex och Grid", "uppgift-3", "./images/uppgift-3.png")
+createCard("Uppgift 4", "Animering i CSS och Youtube-video", "uppgift-4", "./images/uppgift-4.png")
+createCard("Uppgift 5", "Vi gjorde Position", "uppgift-5", "./images/uppgift-5.png")
+createCard("Uppgift 6", "Härmade Johans hemsida", "uppgift-6", "./images/uppgift6.png")
 
 
 // checks if clicked
@@ -38,10 +43,22 @@ isClicked("uppgift-1", "../uppgift-1/index.html")
 isClicked("uppgift-2", "../uppgift-2/index.html")
 isClicked("uppgift-3", "../uppgift-3/index.html")
 isClicked("uppgift-4", "../uppgift-4/index.html")
-isClicked("uppgift-5", "../uppgift-5/index.html")
+isClicked("uppgift-5", "../Uppgift 5/sticky2/index.html")
 isClicked("uppgift-6", "../uppgift-6/index.html")
 
 
+// Responsive Menu
+const hamburgerMenu = document.getElementById("hamburger-menu")
+const menuDiv = document.getElementById("responsive-menu")
+const cancelDiv = document.getElementById("cancel-icon")
+
+hamburgerMenu.addEventListener("click", () => {
+    menuDiv.style.display = 'flex'
+})
+
+cancelDiv.addEventListener("click", () => {
+    menuDiv.style.display = 'none'
+})
 
 
 
